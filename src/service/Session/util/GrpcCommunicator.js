47fbe6a54a5cd0414f7ecda334ad740bdb96f73f
Adam Mitchell <adam@grakn.ai>
2020-05-01 19:24:04 +0100

    Update communicator to use class syntax

--- src/service/Session/util/GrpcCommunicator.js
+++ src/service/Session/util/GrpcCommunicator.js
@@ -16,139 +16,140 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-function SingleResponse(resolve, reject) {
-  this._reject = reject
-  this._resolve = resolve
-}
-
-SingleResponse.prototype._onResponse = function (resp) {
-  this._resolve(resp)
-  return false
-}
-
-SingleResponse.prototype._onError = function (err) {
-  this._reject(err)
-}
-
-function MultiResponse(endEvaluator) {
-  this._responseQueue = []
-  this._readerQueue = []
-  this._endEvaluator = endEvaluator
-}
-
-MultiResponse.prototype.next = function () {
-  return new Promise((resolve, reject) => {
-    const responseQueue = this._responseQueue
-
-    if (responseQueue.length > 0) {
-      resolve(responseQueue.shift())
-    }
-
-    const err = this._error
-    if (err) {
-      reject(err)
-    }
-
-    if (this._finished) {
-      resolve() // Iterator-style empty result for finished stream
-    }
-
-    this._readerQueue.push({resolve, reject})
-  })
-}
-
-MultiResponse.prototype._onResponse = function (response) {
-  const readerQueue = this._readerQueue
-  if (readerQueue.length > 0) {
-    readerQueue.shift().resolve(response)
-  } else {
-    this._responseQueue.push(response)
+class SingleResponse {
+  constructor(resolve, reject) {
+    this._reject = reject
+    this._resolve = resolve
   }
 
-  if (this._endEvaluator(response)) {
-    this._finished = true
+  _onResponse(resp) {
+    this._resolve(resp)
     return false
   }
 
-  return true
+  _onError(err) {
+    this._reject(err)
+  }
 }
 
-MultiResponse.prototype._onError = function (err) {
-  const readerQueue = this._readerQueue
 
-  if (readerQueue === null) {
-    return // Have already received an error
+class MultiResponse {
+  constructor(endEvaluator) {
+    this._responseQueue = []
+    this._readerQueue = []
+    this._endEvaluator = endEvaluator
   }
 
-  for (let reader of readerQueue) {
-    reader.reject(err)
+  next() {
+    return new Promise((resolve, reject) => {
+      const responseQueue = this._responseQueue
+      if (responseQueue.length > 0) {
+        resolve(responseQueue.shift())
+      }
+      const err = this._error
+      if (err) {
+        reject(err)
+      }
+      if (this._finished) {
+        resolve() // Iterator-style empty result for finished stream
+      }
+      this._readerQueue.push({ resolve, reject })
+    })
   }
 
-  this._readerQueue = null // We can never have more readers added after an error
+  _onResponse(response) {
+    const readerQueue = this._readerQueue
+    if (readerQueue.length > 0) {
+      readerQueue.shift().resolve(response)
+    }
+    else {
+      this._responseQueue.push(response)
+    }
+    if (this._endEvaluator(response)) {
+      this._finished = true
+      return false
+    }
+    return true
+  }
 
-  this._finished = true
-  this._error = err
+  _onError(err) {
+    const readerQueue = this._readerQueue
+    if (readerQueue === null) {
+      return // Have already received an error
+    }
+    for (let reader of readerQueue) {
+      reader.reject(err)
+    }
+    this._readerQueue = null // We can never have more readers added after an error
+    this._finished = true
+    this._error = err
+  }
 }
+
 
 /**
  * Wrapper for Duplex Stream that exposes method to send new requests and returns
  * responses as results of Promises.
- * @param {*} stream 
+ * @param {*} stream
  */
-function GrpcCommunicator(stream) {
-  this.stream = stream;
-  this.pending = [];
-
-  this.stream.on('data', resp => {
-    if (!this.pending[0]._onResponse(resp)) {
-      this.pending.shift() // Only remove if resolver returns falsy
-    }
-  });
-
-  this.stream.on('error', err => {
-    this.end();
-    if (this.pending.length) {
-      for (let p of this.pending) {
-        p._onError(err);
+class GrpcCommunicator {
+  constructor(stream) {
+    this.stream = stream
+    this.pending = []
+    this.stream.on('data', resp => {
+      if (!this.pending[0]._onResponse(resp)) {
+        this.pending.shift() // Only remove if resolver returns falsy
       }
-    } else {
-      throw err;
-    }
-  });
+    })
 
-  this.stream.on('status', (e) => {
-    if (this.pending.length) {
-      this.pending.shift()._onError(e);
+    this.stream.on('error', err => {
+      this.end()
+      if (this.pending.length) {
+        for (let p of this.pending) {
+          p._onError(err)
+        }
+      }
+      else {
+        throw err
+      }
+    })
+
+    this.stream.on('status', (e) => {
+      if (this.pending.length) {
+        this.pending.shift()._onError(e)
+      }
+    })
+  }
+
+  send(request) {
+    if (!this.stream.writable)
+      throw 'Transaction is already closed.'
+    return new Promise((resolve, reject) => {
+      this.pending.push(new SingleResponse(resolve, reject))
+      this.stream.write(request)
+    })
+  }
+
+  iterateUntil(request, endEvaluator) {
+    if (!this.stream.writable)
+      throw 'Transaction is already closed.'
+    return new Promise((resolve) => {
+      const responseIterator = new MultiResponse(endEvaluator)
+      this.pending.push(responseIterator)
+      this.stream.write(request)
+      resolve(responseIterator)
+    })
+  }
+
+  end() {
+    if (this.stream.writable) { // transaction is still open
+      this.stream.end()
+      return new Promise((resolve) => {
+        this.stream.on('end', resolve)
+      })
     }
-  })
+  }
 }
 
-GrpcCommunicator.prototype.send = function (request) {
-  if(!this.stream.writable) throw 'Transaction is already closed.';
-  return new Promise((resolve, reject) => {
-    this.pending.push(new SingleResponse(resolve, reject));
-    this.stream.write(request);
-  })
-};
-
-GrpcCommunicator.prototype.iterateUntil = function (request, endEvaluator) {
-  if(!this.stream.writable) throw 'Transaction is already closed.';
-  return new Promise((resolve) => {
-    const responseIterator = new MultiResponse(endEvaluator);
-    this.pending.push(responseIterator);
-    this.stream.write(request);
-    resolve(responseIterator);
-  });
-};
-
-GrpcCommunicator.prototype.end = function end() {
-  if(this.stream.writable) { // transaction is still open
-    this.stream.end();
-    return new Promise((resolve) => {
-      this.stream.on('end', resolve);
-    });
-  }
-};
 
 module.exports = GrpcCommunicator;
