ae395890efb39610ef8ee421882ce21f5d4fb152
Joshua Send <joshuasend1@gmail.com>
2020-08-26 10:35:26 +0100

    close open comment

--- src/GraknClient.js
+++ src/GraknClient.js
@@ -24,7 +24,7 @@ const KeyspaceService = require('./service/Keyspace/KeyspaceService');
 /*
 NOTE: in Grakn 2.0, the protocol has changed, and the locations of the various compiled protobuf files
 have moved (along with rename, etc.)
-*
+*/
 const messages = require("../grpc/nodejs/protocol/session/Session_pb");
 const sessionServices = require("../grpc/nodejs/protocol/session/Session_grpc_pb");
 const keyspaceServices = require("../grpc/nodejs/protocol/keyspace/Keyspace_grpc_pb");
