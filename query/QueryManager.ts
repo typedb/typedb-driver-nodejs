26964c6e6b7dae3f262206ac052c5ee7446cb6ff
Alex Walker <alex@grakn.ai>
2020-11-30 12:18:11 +0000

    Minor refactor in QueryManager

--- query/QueryManager.ts
+++ query/QueryManager.ts
@@ -13,38 +13,37 @@ export class QueryManager {
         this._rpcTransaction = transaction;
     }
 
-    // public match(query: string, options?: GraknOptions): Promise<Transaction.Res> {
-    //     const matchQuery = new Query.Req().setMatchReq(
-    //         new Graql.Match.Req().setQuery(query));
-    //     return this._iterateQuery(matchQuery, options ? options : new GraknOptions(), (res: Transaction.Res) => res.getQueryRes().getMatchRes().getAnswerList());
-    // }
-
+    public match(query: string, options?: GraknOptions): Promise<Transaction.Res> {
+        const matchQuery = new Query.Req().setMatchReq(
+            new Graql.Match.Req().setQuery(query));
+        return this.iterateQuery(matchQuery, options ? options : new GraknOptions(), (res: Transaction.Res) => res.getQueryRes().getMatchRes().getAnswerList());
+    }
 
     public insert(query: string, options?: GraknOptions): Promise<Transaction.Res> {
         const insertQuery = new Query.Req().setInsertReq(
             new Graql.Insert.Req().setQuery(query));
-        return this._iterateQuery(insertQuery, options ? options : new GraknOptions(), (res: Transaction.Res) => res.getQueryRes().getInsertRes().getAnswerList());
+        return this.iterateQuery(insertQuery, options ? options : new GraknOptions(), (res: Transaction.Res) => res.getQueryRes().getInsertRes().getAnswerList());
     }
 
     public delete(query: string, options?: GraknOptions): Promise<void> {
         const deleteQuery = new Query.Req().setDeleteReq(
             new Graql.Delete.Req().setQuery(query));
-        return this._runQuery(deleteQuery, options ? options : new GraknOptions())
+        return this.runQuery(deleteQuery, options ? options : new GraknOptions())
     }
 
     public define(query: string, options?: GraknOptions): Promise<void> {
         const defineQuery = new Query.Req().setDefineReq(
                     new Graql.Define.Req().setQuery(query));
-        return this._runQuery(defineQuery, options ? options : new GraknOptions())
+        return this.runQuery(defineQuery, options ? options : new GraknOptions())
     }
 
     public undefine(query: string, options?: GraknOptions): Promise<void> {
         const undefineQuery = new Query.Req().setUndefineReq(
             new Graql.Undefine.Req().setQuery(query));
-        return this._runQuery(undefineQuery, options ? options : new GraknOptions())
+        return this.runQuery(undefineQuery, options ? options : new GraknOptions())
     }
 
-    private async _runQuery(request: Query.Req, options: GraknOptions): Promise<void> {
+    private async runQuery(request: Query.Req, options: GraknOptions): Promise<void> {
         const transactionRequest = new Transaction.Req()
             .setQueryReq(
                 request.setOptions(ProtoBuilder.options(options))
@@ -52,7 +51,7 @@ export class QueryManager {
         await this._rpcTransaction.execute(transactionRequest);
     }
 
-    private async _iterateQuery(request: Query.Req, options: GraknOptions, responseReader: (res: Transaction.Res) => any): Promise<Transaction.Res> {
+    private async iterateQuery(request: Query.Req, options: GraknOptions, responseReader: (res: Transaction.Res) => any): Promise<Transaction.Res> {
         const transactionRequest = new Transaction.Req()
             .setQueryReq(
                 request.setOptions(ProtoBuilder.options(options))
