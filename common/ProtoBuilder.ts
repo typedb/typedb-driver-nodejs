03ee4069851293b228102bd0a7e383626d9a399f
Alistair Crook <alistair@grakn.ai>
2020-11-30 11:58:43 +0000

    Initial QueryManager definition (ITERATORS NOT GOOD)

--- common/ProtoBuilder.ts
+++ common/ProtoBuilder.ts
@@ -3,11 +3,13 @@ import options_pb from "graknlabs-grpc-protocol/protobuf/options_pb"
 const { Options } = options_pb;
 
 export namespace ProtoBuilder {
-    export function options(options: GraknOptions) {
+    export function options(options?: GraknOptions) {
         let optionsProto = new Options();
-        if (options.infer() != null) optionsProto.setInfer(options.infer() as boolean);
-        if (options.explain() != null) optionsProto.setExplain(options.explain() as boolean);
-        if (options.batchSize() != null) optionsProto.setBatchSize(options.batchSize() as number);
+        if (options) {
+            if (options.infer() != null) optionsProto.setInfer(options.infer() as boolean);
+            if (options.explain() != null) optionsProto.setExplain(options.explain() as boolean);
+            if (options.batchSize() != null) optionsProto.setBatchSize(options.batchSize() as number);
+        }
         return optionsProto;
     }
 }
