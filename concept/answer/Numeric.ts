2bcf7c5870759b2d7bf87584fd8f030c6d0a8178
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-01-06 13:38:30 +0000

    Handle exceptions

--- concept/answer/Numeric.ts
+++ concept/answer/Numeric.ts
@@ -1,5 +1,6 @@
 import * as answer_pb from "grakn-protocol/protobuf/answer_pb";
 import {GraknClientError} from "../../common/errors/GraknClientError";
+import {ErrorMessage} from "../../common/errors/ErrorMessage";
 
 export class Numeric {
     private readonly _numberValue?: number;
@@ -17,7 +18,7 @@ export class Numeric {
             case answer_pb.Numeric.ValueCase.NAN:
                 return Numeric.ofNaN();
             default:
-                throw new GraknClientError("TODO");
+                throw new GraknClientError(ErrorMessage.Query.BAD_ANSWER_TYPE.message(answer.getValueCase()));
         }
     }
 
@@ -31,7 +32,7 @@ export class Numeric {
 
     public asNumber(): number {
         if (this.isNumber()) return this._numberValue;
-        else throw new GraknClientError("TODO");
+        else throw new GraknClientError(ErrorMessage.Query.ILLEGAL_CAST.message("number"));
     }
 
     private static ofNumber(value: number): Numeric {
