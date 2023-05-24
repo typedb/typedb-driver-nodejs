41de766861e0406f0e805a62b86ef71fdca2008a
joshua <joshua@vaticle.com>
2023-05-24 11:16:15 +0100

    Implement Value concept without tests

--- api/concept/Concept.ts
+++ api/concept/Concept.ts
@@ -31,6 +31,8 @@ import { RelationType } from "./type/RelationType";
 import { RoleType } from "./type/RoleType";
 import { ThingType } from "./type/ThingType";
 import { Type } from "./type/Type";
+import {AttributeType as ConceptProto} from "typedb-protocol/common/concept_pb";
+import {Value} from "./value/Value";
 
 export interface Concept {
 
@@ -58,6 +60,8 @@ export interface Concept {
 
     isRelation(): boolean;
 
+    isValue(): boolean;
+
     asType(): Type;
 
     asThingType(): ThingType;
@@ -77,6 +81,8 @@ export interface Concept {
     asAttribute(): Attribute;
 
     asRelation(): Relation;
+
+    asValue(): Value;
 
     equals(concept: Concept): boolean;
 
@@ -110,5 +116,46 @@ export namespace Concept {
         asAttribute(): Attribute.Remote;
 
         asRelation(): Relation.Remote;
+    }
+
+
+    export interface ValueType {
+
+        proto(): ConceptProto.ValueType;
+
+        name(): string;
+    }
+
+    export namespace ValueType {
+
+        class Impl implements ValueType {
+
+            private readonly _proto: ConceptProto.ValueType;
+            private readonly _name: string;
+
+            constructor(type: ConceptProto.ValueType, name: string) {
+                this._proto = type;
+                this._name = name;
+            }
+
+            proto(): ConceptProto.ValueType {
+                return this._proto;
+            }
+
+            name(): string {
+                return this._name.toLowerCase();
+            }
+
+            toString() {
+                return "ValueType[" + this._name + "]";
+            }
+        }
+
+        export const OBJECT = new Impl(ConceptProto.ValueType.OBJECT, "OBJECT");
+        export const BOOLEAN = new Impl(ConceptProto.ValueType.BOOLEAN, "BOOLEAN");
+        export const LONG = new Impl(ConceptProto.ValueType.LONG, "LONG");
+        export const DOUBLE = new Impl(ConceptProto.ValueType.DOUBLE, "DOUBLE");
+        export const STRING = new Impl(ConceptProto.ValueType.STRING, "STRING");
+        export const DATETIME = new Impl(ConceptProto.ValueType.DATETIME, "DATETIME");
     }
 }
